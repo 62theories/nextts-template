36e89f942c0bb96523b62ef1fd87225a0dde298d
62theories <nattawatx86@gmail.com>
2023-02-05 21:26:15 +0700

    revert to next 12

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    loader: "akamai",
+    path: "",
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
